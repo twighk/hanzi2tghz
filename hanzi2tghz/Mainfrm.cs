63c19760debfbe539a6b2c347f84d9e2242598ce
tbranch88@gmail.com <tbranch88@gmail.com@40b4a977-d8fb-63e5-4a47-56c5c411e456>
2013-06-26 17:57:55 +0000

    works again on mac, but almost certainly broken on windows

--- hanzi2tghz/Mainfrm.cs
+++ hanzi2tghz/Mainfrm.cs
@@ -141,33 +141,28 @@ public class Mainfrm : Form
 
     private DialogResult STAShowDialog(FileDialog dialog)
         {
-            DialogState state = new DialogState();
+		
+   				 return dialog.ShowDialog();
+/*            DialogState state = new DialogState();
             state.dialog = dialog;
             System.Threading.Thread t = new System.Threading.Thread(state.ThreadProcShowDialog);
             t.SetApartmentState(System.Threading.ApartmentState.STA);
             t.Start();
             t.Join();
+            
             return state.result;
+*/
         }
 
 
  public class DialogState
-
     {
-
         public DialogResult result;
-
         public FileDialog dialog;
 
-
-        public void ThreadProcShowDialog()
-
-        {
-
+        public void ThreadProcShowDialog(){
             result = dialog.ShowDialog();
-
         }
-
     }
     
 }
